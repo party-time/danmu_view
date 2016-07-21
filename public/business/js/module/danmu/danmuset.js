5438dc62bbac6c57bc330b16285bd93e177c35cf
soaryang <759620299@qq.com>
2016-07-21 15:43:30 +0800

    弹幕审核设置代码修改

--- public/business/js/module/danmu/danmuset.js
+++ public/business/js/module/danmu/danmuset.js
@@ -1,9 +1,16 @@
-testController.controller('danmusetController', ["$scope", function ($scope) {
+testController.controller('danmusetController', ["$scope", "$http",function ($scope, $http) {
 
+
+    /*$scope.pooId = $stateParams.pooId;
+    if ($scope.pooId.$isEmpty()) {
+        $scope.pooId = "578f2ad27d8459ed6656a33e";
+        alert($scope.pooId);
+    }*/
+
+    $scope.pooId = "578f2ad27d8459ed6656a33e";
     $scope.delayTime = 0;
     $scope.dealTime = 0;
     $scope.danmuCount = 0;
-
     //严格度
     $scope.strictstatus = 0;
     $scope.reduceDalyTime = function (value) {
@@ -39,8 +46,76 @@ testController.controller('danmusetController', ["$scope", function ($scope) {
         $scope.danmuCount = $scope.danmuCount + 1;
     }
 
-    $http.get("http://www.runoob.com/try/angularjs/data/Customers_JSON.php")
-        .success(function (response) {
-            $scope.names = response.records;
+    //严格度来控制延迟时间。单条弹幕显示
+    $scope.strictClick = function (value) {
+        $scope.strictstatus = value;
+        if (value == 1) {
+            $("#delayTimeDiv").hide();
+            $("#dealTimeDiv").hide();
+            $scope.dealTime = 0;
+            $scope.delayTime = 0;
+        } else {
+            $("#delayTimeDiv").show();
+            $("#dealTimeDiv").show();
+        }
+    }
+
+    //设置严格度的状态
+    if ($scope.strictstatus == 1) {
+        $("#delayTimeDiv").hide();
+        $("#dealTimeDiv").hide();
+        $scope.dealTime = 0;
+        $scope.delayTime = 0;
+    } else {
+        $("#delayTimeDiv").show();
+        $("#dealTimeDiv").show();
+    }
+
+
+    /*var url = "http://10.2.0.140:8780/danmuConfig/addConfig";
+     var paramObject = {
+     withCredentials:true,
+     strictstatus: $scope.strictstatus,
+     delayTime: $scope.delayTime,
+     dealTime: $scope.dealTime,
+     danmuCount: $scope.danmuCount
+     };
+     $http.post(url, paramObject).success(function (data) {
+     alert(data);
+     });*/
+
+    /**
+     * .controller('ProducersCtrl', function ($scope, $state) { $scope.toProducer = function (producerId) { $state.go('producer', {producerId: producerId}); }; });
+
+     .controller('ProducerCtrl', function ($scope, $state, $stateParams) {
+   var producerId = $stateParams.producerId;
+})
+
+     作者：Ye Huang
+     链接：http://www.zhihu.com/question/33565135/answer/69651500
+     来源：知乎
+     著作权归作者所有，转载请联系作者获得授权。
+     */
+
+    $scope.save = function () {
+        var paramObject = {
+            strictstatus: $scope.strictstatus,
+            pooId:"578f2ad27d8459ed6656a33e",
+            delayTime: $scope.delayTime,
+            dealTime: $scope.dealTime,
+            danmuCount: $scope.danmuCount
+        };
+        $.ajax({
+            type: "post",
+            url: "/bms/danmuConfig/addConfig",
+            data: paramObject,
+            dataType: "json",
+            xhrFields: {
+                withCredentials: true,
+            }
+        }).success(function (data) {
+            alert(data);
         });
+    }
+
 }]);
